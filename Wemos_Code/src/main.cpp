e984b7b0a338dfaa674e802ba25aad2e79063822
Zamtakk <28710885+Zamtakk@users.noreply.github.com>
2020-11-01 13:56:37 +0100

    Updated demo code

--- Wemos_Code/src/main.cpp
+++ Wemos_Code/src/main.cpp
@@ -2,13 +2,15 @@
 
 void setup()
 {
-	// put your setup code here, to run once:
 	Serial.begin(115200);
+	pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
-	// put your main code here, to run repeatedly:
 	Serial.println("Hello world!!");
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(1000);
+	digitalWrite(LED_BUILTIN, LOW);
 	delay(1000);
 }
