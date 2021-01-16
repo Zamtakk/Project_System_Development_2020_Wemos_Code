11287d0ce7b9376996cb8a3caa35da7f9f25001b
Zamtakk <28710885+Zamtakk@users.noreply.github.com>
2021-01-16 13:29:36 +0100

    Move all default functions to default functions file

--- Wemos_Code/src/SimulatedDevice.hpp
+++ Wemos_Code/src/SimulatedDevice.hpp
@@ -1,15 +1,3 @@
-// Include libraries
-#include <Arduino.h>
-
-#include <WebSocketsClient.h>
-#include <ArduinoJson.h>
-#include <Hash.h>
-#include <math.h>
-#include <EEPROM.h>
-
-#include "CommandTypes.hpp"
-#include "DefaultFunctions.hpp"
-
 // Defines
 #define PIN_BUTTON_1 5 //D1
 #define PIN_BUTTON_2 4 //D2
@@ -20,14 +8,7 @@
 
 #define DEVICE_TYPE "SimulatedDevice"
 
-#define HEARTBEAT_INTERVAL 500
-
 // Global variables
-WebSocketsClient webSocket;
-
-char UUID[11];
-
-bool websocketConnected = false;
 
 int LED_1_value = 0;
 int LED_2_value = 0;
@@ -36,22 +17,13 @@ int LED_3_value = 0;
 // Forward Declaration
 
 void initIO();
-void initWifi();
-void initWebsocket();
-
-void websocketEvent(WStype_t type, uint8_t *payload, size_t length);
-
-void handleMessage(JsonObject message);
-void sendIntMessage(int command, int value);
-void sendStringMessage(int command, char *value);
 
 void handleButtons();
 void handlePotmeter();
 void handleLEDs();
 
-void sendHeartbeat();
-
-void generateUUID();
+#include "CommandTypes.hpp"
+#include "DefaultFunctions.hpp"
 
 // Setup
 void setup()
@@ -95,73 +67,6 @@ void initIO()
     pinMode(PIN_LED_1, OUTPUT);
     pinMode(PIN_LED_2, OUTPUT);
     pinMode(PIN_LED_3, OUTPUT);
-}
-
-/*!
-    @brief Starts the Websocket Client and connects with the server.
-*/
-void initWebsocket()
-{
-    // server address, port and URL
-    webSocket.begin("10.0.1.1", 9002, "/");
-
-    // event handler
-    webSocket.onEvent(websocketEvent);
-
-    // try ever 5000 again if connection has failed
-    webSocket.setReconnectInterval(2000);
-}
-
-/*!
-    @brief Interrupt that is called when a new message is received by the websocket client
-    @param[in] type Event type that is invoked, ex. CONNECTED, DISCONNECTED or new TEXT
-    @param[in] payload A pointer to the payload of the message, in this case a JSON message
-    @param[in] length Length of the message that the pointer is pointing to.
-*/
-void websocketEvent(WStype_t type, uint8_t *payload, size_t length)
-{
-    switch (type)
-    {
-    case WStype_DISCONNECTED:
-        if (websocketConnected)
-        {
-            Serial.printf("[Websocket] Disconnected!\n");
-            websocketConnected = false;
-        }
-        break;
-    case WStype_CONNECTED:
-    {
-        Serial.printf("[Websocket] Connected!\n");
-        websocketConnected = true;
-
-        StaticJsonDocument<200> message;
-        char stringMessage[200];
-
-        message["UUID"] = UUID;
-        message["Type"] = DEVICE_TYPE;
-        message["command"] = REGISTRATION;
-        message["value"] = "";
-
-        serializeJson(message, stringMessage);
-
-        Serial.printf("[Websocket] Sending registration: %s\n", stringMessage);
-        webSocket.sendTXT(stringMessage);
-        break;
-    }
-    case WStype_TEXT:
-    {
-        Serial.printf("[Websocket] New message: %s\n", payload);
-
-        DynamicJsonDocument doc(1024);
-        deserializeJson(doc, payload);
-        JsonObject obj = doc.as<JsonObject>();
-
-        handleMessage(obj);
-        break;
-    }
-    default:
-        break;
-    }
 }
 
 /*!
@@ -214,69 +119,6 @@ void handleMessage(JsonObject message)
         Serial.printf("[Error] Unsupported command received: %d\n", (int)message["command"]);
         break;
     }
-}
-
-/*!
-    @brief Sends a new message over the Websocket connection with an integer as JSON value
-    @param[in] command The command send in the JSON packet, see CommandTypes.hpp
-    @param[in] value An integer value that is send with the command as parameter.
-*/
-void sendIntMessage(int command, int value)
-{
-    StaticJsonDocument<200> message;
-    char stringMessage[200];
-
-    message["UUID"] = UUID;
-    message["Type"] = DEVICE_TYPE;
-    message["command"] = command;
-    message["value"] = value;
-
-    serializeJson(message, stringMessage);
-
-    Serial.printf("Sending message: %s\n", stringMessage);
-    webSocket.sendTXT(stringMessage);
-}
-
-/*!
-    @brief Sends a new message over the Websocket connection with an bool as JSON value
-    @param[in] command The command send in the JSON packet, see CommandTypes.hpp
-    @param[in] value A boolean value that is send with the command as parameter.
-*/
-void sendBoolMessage(int command, bool value)
-{
-    StaticJsonDocument<200> message;
-    char stringMessage[200];
-
-    message["UUID"] = UUID;
-    message["Type"] = DEVICE_TYPE;
-    message["command"] = command;
-    message["value"] = value;
-
-    serializeJson(message, stringMessage);
-
-    Serial.printf("Sending message: %s\n", stringMessage);
-    webSocket.sendTXT(stringMessage);
-}
-
-/*!
-    @brief Sends a new message over the Websocket connection with an string as JSON value
-    @param[in] command The command send in the JSON packet, see CommandTypes.hpp
-    @param[in] value A string value that is send with the command as parameter.
-*/
-void sendStringMessage(int command, char *value)
-{
-    StaticJsonDocument<200> message;
-    char stringMessage[200];
-
-    message["UUID"] = UUID;
-    message["Type"] = DEVICE_TYPE;
-    message["command"] = command;
-    message["value"] = value;
-
-    serializeJson(message, stringMessage);
-
-    Serial.printf("Sending message: %s\n", stringMessage);
-    webSocket.sendTXT(stringMessage);
 }
 
 /*!
@@ -361,75 +203,4 @@ void handleLEDs()
         analogWrite(PIN_LED_3, LED_3_value);
         Serial.printf("Led 3 updated to %d!\n", LED_3_value);
     }
-}
-
-/*!
-    @brief Check if the heartbeat interval time has passed and then send a heartbeat
-*/
-void sendHeartbeat()
-{
-    static uint32_t lastTime = 0;
-
-    if ((millis() - lastTime) > HEARTBEAT_INTERVAL)
-    {
-        lastTime = millis();
-        if (websocketConnected)
-        {
-            StaticJsonDocument<200> message;
-            char stringMessage[200];
-
-            message["UUID"] = UUID;
-            message["Type"] = DEVICE_TYPE;
-            message["command"] = HEARTBEAT;
-
-            serializeJson(message, stringMessage);
-
-            webSocket.sendTXT(stringMessage);
-        }
-    }
-}
-
-/*!
-    @brief Read the UUID from EEPROM or generates a new one if none is present
-*/
-void generateUUID()
-{
-    uint addr = 0;
-
-    struct
-    {
-        uint8_t code[3];
-        char uuid[11] = "";
-    } data;
-
-    EEPROM.begin(512);
-    EEPROM.get(addr, data);
-
-    if (data.code[0] == 34 && data.code[1] == 42 && data.code[2] == 16)
-    {
-        strcpy(UUID, data.uuid);
-        Serial.printf("[SETUP] UUID loaded! ID is: %s\n", UUID);
-        return;
-    }
-    else
-    {
-        randomSeed(ESP.getCycleCount());
-
-        char buffer[1];
-        for (int i = 0; i < 10; i++)
-        {
-            itoa(random(9), buffer, 10);
-            UUID[i] = buffer[0];
-        }
-        UUID[10] = '\0';
-        Serial.printf("[SETUP] New UUID generated! New ID is: %s\n", UUID);
-
-        data.code[0] = 34;
-        data.code[1] = 42;
-        data.code[2] = 16;
-        strcpy(data.uuid, UUID);
-    }
-
-    EEPROM.put(addr, data);
-    EEPROM.commit();
 }
